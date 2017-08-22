0557f02b5892cfcb899150da6dd02864b8d798ef
yaoyizhi <email@yaoadou888>
2017-08-22 17:28:39 +0800

    14-prime

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,14 +1,25 @@
 # 输入一个数字 N，请检查是不是质数
 
-def is_prime(n)
-# ....
-end
-
-print "请输入数字 N，然后按 Enter: "
+print '请输入数字 N，然后按 Enter: '
 n = gets
 
-if is_prime(n.to_i)
-  puts "这是质数"
+def is_prime(n)
+  if n.to_i > 1 && n.to_i <= 3
+    true
+  elsif n.to_i > 3
+    i = 2
+    while i <= n.to_i / 2
+      if n.to_i % i == 0
+        return false
+        break
+       end
+      i += 1
+    end
+    true
+end
+end
+if is_prime(n)
+  puts '这是质数'
 else
-  puts "这不是质数"
+  puts '这不是质数'
 end
