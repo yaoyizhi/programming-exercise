e0e9e866e1a43c735db92fbfa677cdd24c65c7a7
yaoyizhi <email@yaoadou888>
2017-08-22 09:14:10 +0800

    10

--- 09-function.rb
+++ 09-function.rb
@@ -1,15 +1,13 @@
 # 题目: 输入直角三角形的宽和高，输出三角形的面积
 
-def calculate_area(a, b)
-  # ....
-end
-
-print "请输入直角三角形的高，然后按 Enter: "
+print '请输入直角三角形的高，然后按 Enter: '
 a = gets
 
-print "请输入直角三角形的底边，然后按 Enter: "
+print '请输入直角三角形的底边，然后按 Enter: '
 b = gets
 
-answer = calculate_area(a,b)
-
+def calculate_area(a, b)
+  a.to_i * b.to_i / 2
+end
+answer = calculate_area(a, b)
 puts "直角三角形的面积是: #{answer}"
