ebeb02811af59e23c9791f825cc8e7b213f32a1f
yaoyizhi <email@yaoadou888>
2017-08-23 16:11:22 +0800

    17-array-stats

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -2,8 +2,8 @@
 
 arr = []
 
-while (true)
-  print "请输入数字，结束请直接按 Enter: "
+loop do
+  print '请输入数字，结束请直接按 Enter: '
   user_input = gets
   if user_input == "\n"
     break
@@ -12,9 +12,15 @@ while (true)
   end
 end
 
+def total(array)
+  total = 0
+  array.each do |i|
+    total += i
+  end
+  total
+end
 puts arr.to_s
-
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+puts "总和是#{total(arr)}"
+puts "平均值是#{total(arr) / arr.size}"
+puts "最大值是#{arr.max}"
+puts "最小值是#{arr.min}"
