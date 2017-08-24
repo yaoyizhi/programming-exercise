711254c6f23633e9531a67085e89611f6d787a57
yaoyizhi <email@yaoadou888>
2017-08-24 17:03:37 +0800

    28-word-count

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,10 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
-doc = File.read("wordcount.txt")
-
+doc = File.read('wordcount.txt')
+arr = doc.split('')
+h = {}
+arr.each do |i|
+  h[i] = arr.count(i)
+end
+puts h
 # ...
