12e6c510f6ece9f72e2189868f314b37ddefe1f0
yaoyizhi <email@yaoadou888>
2017-08-24 17:40:19 +0800

    29-todos

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,6 +1,6 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
-doc = File.read('wordcount.txt')
+doc = File.read('wordcount.txt') # 每个字母出现的次数
 arr = doc.split('')
 h = {}
 arr.each do |i|
@@ -8,3 +8,10 @@ arr.each do |i|
 end
 puts h
 # ...
+# 每个单词出现的次数
+
+doc = File.read('wordcount.txt')
+words = doc.downcase.scan(/\w+/)
+words.uniq.each do |i|
+  puts "#{i} 出现#{words.count(i)}次"
+end
