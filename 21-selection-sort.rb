0da719666032bc4044e4ded98f8b1c67e2504420
yaoyizhi <email@yaoadou888>
2017-08-25 15:00:16 +0800

    21-selection

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,14 +1,28 @@
 # 给定一数组内含数字，请实作选择排序法进行排序。
 # https://zh.wikipedia.org/wiki/选择排序
 
-def selection_sort(arr)
-  arr_new = []
-  arr.each_with_index do |i|
-    arr_new << arr.min if arr.min = i
-  end
-  arr_new
-end
+# def selection_sort(arr) 这是错误的写法
+# arr_new = []
+# arr.each_with_index do |i|
+# arr_new << arr.min if arr.min = i
+# end
+# arr_new
+# end
+def selection_sort(arr) # 这个写法不理解
+  (0..arr.length - 1).each do |i|
+    min = arr[i]
+    index = i
 
+    (i + 1..arr.length - 1).each do |j|
+      if arr[j] < min
+        min = arr[j]
+        index = j
+      end
+    end
+    arr[i], arr[index] = arr[index], arr[i]
+  end
+  arr
+end
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
 answer = selection_sort(arr)
